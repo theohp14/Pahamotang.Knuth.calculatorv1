339bff66d3c17e02738deb077e8b2be51dfd833b
TheoLenuelPahamotang <theolenuel@gmail.com>
2024-02-25 19:46:06 +0800

    calculator version 1

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,15 +1,13 @@
 package com.example.calculator;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-publicpublic class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
     TextView txtResult;
     EditText txtNum1, txtNum2;
     Button btnAdd, btnSub, btnMul, btnDiv;
@@ -27,52 +25,39 @@ publicpublic class MainActivity extends AppCompatActivity {
         btnMul = (Button) findViewById(R.id.button3);
         btnDiv = (Button) findViewById(R.id.button4);
 
-        btnAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int myNum1 = Integer.parseInt(txtNum1.getText().toString());
-                int myNum2 = Integer.parseInt(txtNum2.getText().toString());
-                int sum = myNum1 + myNum2;
+        btnAdd.setOnClickListener(v -> {
+            int myNum1 = Integer.parseInt(txtNum1.getText().toString());
+            int myNum2 = Integer.parseInt(txtNum2.getText().toString());
+            int sum = myNum1 + myNum2;
 
-                txtResult.setText(String.valueOf(sum));
-            }
+            txtResult.setText(String.valueOf(sum));
         });
-        btnSub.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        btnSub.setOnClickListener(v -> {
 
-                double myNum1 = Double.parseDouble(txtNum1.getText().toString());
-                double myNum2 = Double.parseDouble(txtNum2.getText().toString());
-                double dif = myNum1 - myNum2;
+            double myNum1 = Double.parseDouble(txtNum1.getText().toString());
+            double myNum2 = Double.parseDouble(txtNum2.getText().toString());
+            double dif = myNum1 - myNum2;
 
-                txtResult.setText(String.valueOf(dif));
-            }
+            txtResult.setText(String.valueOf(dif));
         });
-        btnMul.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        btnMul.setOnClickListener(v -> {
 
-                double myNum1 = Double.parseDouble(txtNum1.getText().toString());
-                double myNum2 = Double.parseDouble(txtNum2.getText().toString());
-                double product = myNum1 * myNum2;
+            double myNum1 = Double.parseDouble(txtNum1.getText().toString());
+            double myNum2 = Double.parseDouble(txtNum2.getText().toString());
+            double product = myNum1 * myNum2;
 
-                txtResult.setText(String.valueOf(product));
-            }
+            txtResult.setText(String.valueOf(product));
         });
-        btnDiv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        btnDiv.setOnClickListener(v -> {
 
-                double myNum1 = Double.parseDouble(txtNum1.getText().toString());
-                double myNum2 = Double.parseDouble(txtNum2.getText().toString());
-                double quotient = myNum1 / myNum2;
+            double myNum1 = Double.parseDouble(txtNum1.getText().toString());
+            double myNum2 = Double.parseDouble(txtNum2.getText().toString());
+            double quotient = myNum1 / myNum2;
 
-                txtResult.setText(String.valueOf(quotient));
-            }
+            txtResult.setText(String.valueOf(quotient));
         });
     }
-
-
+}
 
 
 
